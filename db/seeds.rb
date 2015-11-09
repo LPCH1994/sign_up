b163bef766a5b3775ad467513392c2143ba4af1c
LPCH1994 <hsblh32@gmail.com>
2015-11-09 20:19:29 +0700

    crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "hsblh32",
+             email: "hsblh32@gmail.com",
+             password:              "12341234",
+             password_confirmation: "12341234",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "test#{n+1}@gmail.com"
+  password = "123456"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
