4b4de375b9ec8b3db0ed563e487a775df28b8c7d
LPCH1994 <hsblh32@gmail.com>
2015-11-16 21:19:38 +0700

    Add acc+pass+email

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "hsblh32@gmail.com"
+  layout 'mailer'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "hsblh32",
              email: "hsblh32@gmail.com",
              password:              "12341234",
              password_confirmation: "12341234",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 99.times do |n|
   name  = Faker::Name.name
@@ -11,5 +21,7 @@ User.create!(name:  "hsblh32",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
