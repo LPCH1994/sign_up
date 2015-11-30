fcebde6a7f72fd4144e489e66440ad7df1923c63
LPCH1994 <hsblh32@gmail.com>
2015-12-01 03:35:41 +0700

    tut done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'hieu-sign-up.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['lpch1994'],
+    :password       => ENV['unlock123'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
